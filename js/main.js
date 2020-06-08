7e82492d6c07cfc1b3fd148b03aa4c9fdd625e45
Lexa070301 <Lexa070301@mail.ru>
2020-06-08 09:19:18 +0300

    add last news and news board

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
-    
+    var last_news = new Swiper ('.last_news-container', {
+        direction: 'horizontal',
+        loop: true,
+        navigation: {
+            nextEl: '.last_news-next',
+            prevEl: '.last_news-prev',
+        },
+        autoplay: {
+            delay: 5000,
+        }
+    });
+    $('.board__grid').masonry({
+        itemSelector: '.board__grid__item',
+        columnWidth: 300,
+        gutter: 30,
+        fitWidth: true
+    });
 });
