68576ff34ce53736a43d8e92c04183fbbc1160bc
Lexa070301 <Lexa070301@mail.ru>
2020-06-09 14:22:20 +0300

    add cabinet page and custom scroll bar

--- js/main.js
+++ js/main.js
@@ -34,22 +34,23 @@ $(document).ready(function () {
         $('[data-remodal-id=cabinet__enter__modal]').remodal().open();
     });
     $('[type="password"]').on('keyup', function () {
-        if ($('#cabinet__registration__form [name="password"]').val() == $('#cabinet__registration__form [name="password2"]').val()) {
-            $('#cabinet__registration__form [type="password"]').removeClass('red-border');
+        if ($('.cabinet__registration__form [name="password"]').val() == $('.cabinet__registration__form' +
+            ' [name="password2"]').val()) {
+            $('.cabinet__registration__form [type="password"]').removeClass('red-border');
 
         } else {
-            $('#cabinet__registration__form [type="password"]').addClass('red-border');
+            $('.cabinet__registration__form [type="password"]').addClass('red-border');
         }
     });
-    $('#cabinet__registration__form .form-input').on('keyup', function () {
-        if (($('#cabinet__registration__form [name="name"]').val() != '') && ($('#cabinet__registration__form' +
-            ' [name="email"]').val() != '') && ($('#cabinet__registration__form [name="password"]').val() != '') && ($('#cabinet__registration__form [name="password2"]').val() != '') && ($('#cabinet__registration__form [name="password"]').val() == $('#cabinet__registration__form [name="password2"]').val())) {
-            $('#cabinet__registration__form .remodal-confirm').removeAttr("disabled");
+    $('.cabinet__registration__form .form-input').on('keyup', function () {
+        if (($('.cabinet__registration__form [name="name"]').val() != '') && ($('#cabinet__registration__form' +
+            ' [name="email"]').val() != '') && ($('.cabinet__registration__form [name="password"]').val() != '') && ($('.cabinet__registration__form [name="password2"]').val() != '') && ($('.cabinet__registration__form [name="password"]').val() == $('.cabinet__registration__form [name="password2"]').val())) {
+            $('.cabinet__registration__form .remodal-confirm').removeAttr("disabled");
         }
     });
-    $('#cabinet__enter__form .form-input').on('keyup', function () {
-        if (($('#cabinet__enter__form [name="email"]').val() != '') && ($('#cabinet__enter__form [name="password"]').val() != '')) {
-            $('#cabinet__enter__form .remodal-confirm').removeAttr("disabled");
+    $('.cabinet__enter__form .form-input').on('keyup', function () {
+        if (($('.cabinet__enter__form [name="email"]').val() != '') && ($('.cabinet__enter__form [name="password"]').val() != '')) {
+            $('.cabinet__enter__form .remodal-confirm').removeAttr("disabled");
         }
     });
 });
