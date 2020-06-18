59fc35629c0375761d2991b6d46cc009554ad5fa
Lexa070301 <Lexa070301@mail.ru>
2020-06-18 21:03:14 +0300

    add menu and scrolling script

--- lending/js/main.js
+++ lending/js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
     var scene = document.getElementById('scene');
     var parallaxInstance = new Parallax(scene);
+    new WOW().init();
+    $('header__menu__link').on( 'click', function(){
+        var el = $(this);
+        var dest = el.attr('href'); // получаем направление
+        if(dest !== undefined && dest !== '') { // проверяем существование
+            $('html').animate({
+                    scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+                }, 1000 // скорость прокрутки
+            );
+        }
+        return false;
+    });
 });
