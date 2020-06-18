68fe9f83968a1cce39793308790051f6317acba0
Lexa070301 <Lexa070301@mail.ru>
2020-06-18 21:58:21 +0300

    desktop goal section is ready

--- lending/js/main.js
+++ lending/js/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     var scene = document.getElementById('scene');
     var parallaxInstance = new Parallax(scene);
+    var img_scene = document.getElementById('goal__img-scene');
+    var parallaxInstance = new Parallax(img_scene);
     new WOW().init();
-    $('header__menu__link').on( 'click', function(){
+    $('.header__menu__link').on( 'click', function(){
         var el = $(this);
         var dest = el.attr('href'); // получаем направление
         if(dest !== undefined && dest !== '') { // проверяем существование
